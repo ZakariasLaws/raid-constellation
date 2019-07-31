88e28cdedfa38f619aaa46db883660e051727874
Zakarias Laws <zakarias.laws@arm.com>
2019-07-31 15:12:01 -0700

    Added configuration script

---
+++ configuration/configure.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo "NOTE: Run configuration script from root of git as such: ./configuration/config.sh"
+
+function isNumber {
+  re='^[0-9]+$'
+  if ! [[ $1 =~ $re ]] ; then
+    echo "$1 is not a valid port number" >&2
+    exit 1
+  fi
+}
+
+echo "This script will set the necessary configurations, the config file will be placed in the provided bin dir."
+
+read -p "Constellation port: " constPort
+
+isNumber $constPort
+
+read -ep "Edgeinference build location: " binDir
+
+if ! [[ -d $binDir ]]; then
+  echo "$binDir is not a directory"
+  exit 1
+fi  
+
+read -ep "TensorFlow serving config: " tfConf
+
+if ! [[ -f $tfConf ]]; then
+  echo "$tfConf does not exist"
+  exit 1
+fi
+
+read -p "TensorFlow serving port: " tfPort
+
+isNumber $tfPort
+
+CONFIG_FILE="configuration/config.RAID"
+
+echo "CONSTELLATION_PORT=$constPort" > $CONFIG_FILE
+echo "EDGEINFERENCE_DIR=$binDir" >> $CONFIG_FILE
+echo "EDGEINFERENCE_SERVING_PORT=$tfPort" >> $CONFIG_FILE
+echo "EDGEINFERENCE_SERVING_CONFIG=$tfConf" >> $CONFIG_FILE
+
+echo "Created config file succesfully"
