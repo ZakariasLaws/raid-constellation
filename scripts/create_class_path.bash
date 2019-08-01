998653575a893bb0ba708178ddf02ae026de6ee9
Zakarias Laws <zakarias.laws@arm.com>
2019-08-01 14:06:55 -0700

    removed file by mistake

---
+++ scripts/create_class_path.bash
@@ -0,0 +1,17 @@
+
+#!/bin/bash
+
+createClassPath() {
+    basedir=$1
+    jar=$2
+    classpath="$1/$2"
+
+    if [[ -d ${basedir}/lib ]]
+    then
+	for i in ${basedir}/lib/*.jar
+	do
+	    classpath+=":$i"
+	done
+    fi
+    echo ${classpath}
+}
