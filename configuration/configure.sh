b36d1a5b3b855a70835a1863951458acfcf0e209
Zakarias Laws <zakarias.nl@gmail.com>
2020-01-07 19:20:10 -0800

    Updated config script to better detect errors

--- configuration/configure.sh
+++ configuration/configure.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
+function check_env() {
+    local name_env_dir=$1
+    if [[ -z ${!name_env_dir} ]]
+    then
+	echo "Environment variable $name_env_dir has not been set"
+	exit 1
+    fi
+}
+
+function check_env_dir() {
+    local name_env_dir=$1
+
+    check_env ${name_env_dir}
+
+    if [[ ! -d ${!name_env_dir} ]]
+    then
+	echo "Environment variable $name_env_dir does not represent a directory"
+	exit 1
+    fi
+}
+
 #DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "NOTE: Run configuration script from root of git as such: ./configuration/config.sh"
+
+check_env_dir RAID_DIR
 
 function isNumber {
   re='^[0-9]+$'
@@ -39,5 +62,11 @@ CONFIG_FILE="${RAID_DIR}/config.RAID"
 echo "CONSTELLATION_PORT=$constPort" > $CONFIG_FILE
 echo "TENSORFLOW_BIN=$tfBin" >> $CONFIG_FILE
 echo "TENSORFLOW_SERVING_CONFIG=${tfConf}" >> $CONFIG_FILE
+
+
+if [[ ! -f $CONFIG_FILE ]]; then
+    echo "Something went wrong, check environment variables and try again"
+    exit 1
+fi
 
 echo "Created config file successfully"
